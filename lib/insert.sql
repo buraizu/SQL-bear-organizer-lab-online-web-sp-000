50dfd602b071bf08b97995fdac50c66fbd49d224
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-27 23:52:33 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'Male', 'Dark Brown', 'Sweet', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, 'Female', 'Light Brown', 'Rambunctious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'Female', 'Dark Brown', 'Shy', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'Male', 'Brown', 'Aggressive', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, 'Female', 'Dark Brown', 'Grouchy', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 4, 'Male', 'Light Brown', 'Playful', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 10, 'Female', 'Brown', 'Aggressive', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 12, 'Male', 'Brown', 'Hungry', false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'Male', 'Dark Brown', 'Sweet', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, 'Female', 'Light Brown', 'Rambunctious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'Female', 'Dark Brown', 'Shy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'Male', 'Brown', 'Aggressive', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, 'Female', 'Dark Brown', 'Grouchy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 4, 'Male', 'Light Brown', 'Playful', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 10, 'Female', 'Brown', 'Aggressive', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 12, 'Male', 'Brown', 'Hungry', 1);
