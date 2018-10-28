262def7557c42ca385afe8974d8f388e1c805b0d
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-28 00:08:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,13 +32,13 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  ""
+  "SELECT color
+  FROM bears
+  GROUP BY color"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperament)
-  FROM bears
-  WHERE temperament = 'Goofy'"
+  ""
 end
 
 def selects_bear_that_killed_Tim
