7aa223a98a59b16632c5ae6eea0b767948bb18ab
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-28 00:24:43 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,10 +33,10 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   "SELECT color, COUNT(color) AS count
-  FROM bears
-  GROUP BY color
-  ORDER BY count DESC
-  LIMIT 1"
+    FROM bears
+    GROUP BY color
+    ORDER BY count DESC
+    LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
